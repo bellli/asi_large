b148b15f4cbf20c08b8389e9facf556981f9d479
bellli <bellli@.bk.ru>
2023-10-27 19:19:17 +0300

    init

---
+++ mod_info.lua
@@ -0,0 +1,10 @@
+name = "asi_large"
+uid = "asi_large-ui-mod-icons-2"
+version = 2
+
+description = " nothing was cut, nothing was changed, mod required restart faf after install mod"
+copyright = ""
+author = "lamobot"
+
+ui_only = true
+icon = "/mods/asi_large/icon.png"
